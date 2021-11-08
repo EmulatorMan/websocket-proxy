2e74315e8353ed0c6b152c44077aee5beaf93345
Caracal.js <59297610+caracal-js@users.noreply.github.com>
2021-11-07 22:12:58 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const Corrosion = require('corrosion');
+const http = require('http');
+const proxy = new Corrosion({
+      prefix: '/service/',
+      codec: 'xor',
+})
+
+http.createServer().on('upgrade', proxy.upgrade).listen(8080);
